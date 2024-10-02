d218daca8492ca0393cafd3896bdf89bf18d7b9f
Ruan Meyer <rmeyer.dev@gmail.com>
2024-10-02 16:38:00 +0100

    act5

--- JavaApplication1/src/javaapplication1/Act6.java
+++ JavaApplication1/src/javaapplication1/Act6.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Act6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
     }
 }
