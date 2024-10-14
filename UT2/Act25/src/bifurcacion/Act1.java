ec3742cd29d354f30e5551584f402f7dbd56208a
Ruan Meyer <rmeyer.dev@gmail.com>
2024-10-15 00:24:15 +0100

    aaaaaaa

---
+++ UT2/Act25/src/bifurcacion/Act1.java
@@ -0,0 +1,20 @@
+package bifurcacion;
+
+import java.util.Scanner;
+
+public class Act1 {
+    public static void main(String[] Args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entra el primer numero:");
+        int number1 = scanner.nextInt();
+        
+        System.out.println("Entra el segundo numero;");
+        int number2 = scanner.nextInt();
+        
+        if(number1 > number2) {
+            System.out.println("La suma de los dos numeros es " + (number1+number2));
+            System.out.println("El producto de los dos numeros es ");
+        }
+    }
+}
