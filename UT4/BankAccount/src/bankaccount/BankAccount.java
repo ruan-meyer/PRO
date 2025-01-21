744c405e2dff073a6291b32c1f7f5d9b53defc63
Ruan Meyer <rmeyer.dev@gmail.com>
2025-01-21 11:46:10 +0000

    Bank system

--- UT4/BankAccount/src/bankaccount/BankAccount.java
+++ UT4/BankAccount/src/bankaccount/BankAccount.java
@@ -10,35 +10,35 @@ public class BankAccount {
    
     //<editor-fold defaultstate="collapsed" desc="Getters & Setters">
         private void setBalance(double value){
-            balance = balance + value;
+            this.balance = this.balance + value;
         }
 
         public double getBalance(){
-            return balance;
+            return this.balance;
         }
 
         private void setHolderName(String value){
-            holderName = value;
+            this.holderName = value;
         }
         
         public String getHolderName(){
-            return holderName;
+            return this.holderName;
         }
         
         private void setHolderDNI(String value){
-            holderDNI = value;
+            this.holderDNI = value;
         }
         
         public String getHolderDNI(){
-            return holderDNI;
+            return this.holderDNI;
         }
         
         private void setPin(int value){
-            pin = value;
+            this.pin = value;
         }
         
         private int getPin(){
-            return pin;
+            return this.pin;
         }
     //</editor-fold>
         
@@ -67,22 +67,128 @@ public class BankAccount {
         System.out.print("Enter the PIN: ");
         setPin(sc.nextInt());
         System.out.println("");
+        
+        //Final message
+        System.out.println("Account created successfully");
     }
     //</editor-fold>
         
     //<editor-fold defaultstate="collapsed" desc="Methods">
-        public boolean checkPin(int value){
+        public boolean checkPin(){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the PIN: ");
+            int value = sc.nextInt();
+            
             if(getPin() == value){
                 return true;
-            } 
+            }
+            System.out.println("Invalid PIN");
             return false;
         }
         
         public void changePin(){
+            if(!checkPin()){
+                return;
+            }
+            
             Scanner sc = new Scanner(System.in);
-            
+
+            int[] newPin = new int[2];
             System.out.print("Enter the new PIN: ");
+            newPin[0] = sc.nextInt();
+            System.out.println("");
+
+            System.out.print("Enter the new PIN again: ");
+            newPin[1] = sc.nextInt();
+            System.out.println("");
+
+            while(newPin[0] != newPin[1]){
+                System.out.println("The PINs doesn't match");
+                System.out.print("Enter 1 to try again or 2 to exit:");
+                int exit = sc.nextInt(); 
+                if(exit == 2){
+                    return;
+                }
+                System.out.print("Enter the new PIN: ");
+                newPin[0] = sc.nextInt();
+                System.out.println("");
+
+                System.out.print("Enter the PIN again: ");
+                newPin[1] = sc.nextInt();
+                System.out.println("");
+            }
+            setPin(newPin[0]);
+        }
+        
+        public void deposit(){
+            if(!checkPin()){
+                return;
+            }
+                        
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the amount of money to deposit: ");
+            double value = sc.nextDouble();
             
+            if(value > 0){
+                setBalance(getBalance() + value);
+                System.out.println("Deposit completed successfully");
+                return;
+            }
+            while(value <= 0){
+                System.out.print("Invalid amount, enter the amount again or enter 0 to exit");
+                value = sc.nextDouble();
+                if(value == 0){
+                    System.out.println();
+                    return;
+                }
+                if(value > 0){
+                    setBalance(getBalance() + value);
+                    System.out.println("Deposit completed successfully");
+                    return;
+                }
+            }
+        }
+        
+        public void withdraw(){
+            if(!checkPin()){
+                return;
+            }
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Balance: " + getBalance() + "€");
+            System.out.print("Enter the amount of money to withdraw: ");
+            double value = sc.nextDouble();
+            
+            if(value > 0 && value <= getBalance()){
+                setBalance(getBalance() - value);
+                System.out.println("Withdraw completed successfully");
+                return;
+            }
+            while(value <= 0 || value >= getBalance()){
+                System.out.println("\nBalance: " + getBalance() + "€");
+                System.out.print("Invalid amount, enter the amount again or enter 0 to exit");
+                if(value == 0){
+                    System.out.println();
+                    return;
+                }
+                if(value > 0 && value <= getBalance()){
+                    setBalance(getBalance() + value);
+                    System.out.println("Withdraw completed successfully");
+                    return;
+                }
+            }
+        }
+        
+        public void changeHolderName(){
+            if(!checkPin()){
+                return;
+            }
+                        
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the new holders name: ");
+            String newName = sc.nextLine();
+            setHolderName(newName);
+            System.out.println("Name changed successfully");
         }
     //</editor-fold>
 }
--- UT4/BankAccount/src/bankaccount/Main.java
+++ UT4/BankAccount/src/bankaccount/Main.java
@@ -1,7 +1,120 @@
 package bankaccount;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        BankAccount acc = new BankAccount("Ruan", "d234533", 455);
+        Scanner sc = new Scanner(System.in);
+        
+        BankAccount[] accounts = new BankAccount[500];
+        int numberAccounts = 0;
+        
+        while(true){
+            int opt = 0;
+            System.out.println("""
+                               Welcome to Maze Bank 
+                               What would you like to do? 
+                               
+                               1 - Create a new account 
+                               2 - Log in into an existent account
+                               3 - Exit """);
+            System.out.print("Enter your option [1-3]: ");
+            opt = sc.nextInt();
+            
+            switch(opt){
+                case 1:
+                   accounts[numberAccounts] = createAccount();
+                case 2:
+                    System.out.print("Enter your DNI: ");
+                    String dni = sc.next();
+                    // IMPLEMENT WARN ABOUT INEXISTENT ACCOUNT
+                    for (int i = 0; i <= accounts.length; i++){
+                        if(dni.equals(accounts[i].getHolderDNI())){
+                            if(accounts[i].checkPin()){
+                                accountMenu(accounts[i]);
+                            } else {
+                                System.out.print("Wrong PIN, press 1 to try again or 2 to exit");
+                                int exit = sc.nextInt();
+                                while(exit == 1){
+                                    if(accounts[i].checkPin()){
+                                        accountMenu(accounts[i]);
+                                        break;
+                                    }
+                                    System.out.print("Wrong PIN, press 1 to try again or 2 to exit");
+                                    exit = sc.nextInt();
+                                }
+                            }
+                        }
+                    }
+                case 3:
+            }
+        }  
+    }
+    
+    public static BankAccount createAccount(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Creating an account...");
+        System.out.println(" ");
+        BankAccount acc = new BankAccount();
+        System.out.print("Press any key to move forward:");
+        sc.next();
+        Utils.clearScreen();
+        
+        return acc;
+    }
+    
+    public static void accountMenu(BankAccount acc){
+        Scanner sc = new Scanner(System.in);
+        
+        int opt = 0;
+        while(opt != 6){
+            System.out.println("Hello, " + acc.getHolderName() + "!");
+            System.out.println("""
+                               Welcome to Maze Bank 
+                               What would you like to do? 
+                               
+                               1 - See balance 
+                               2 - Deposit 
+                               3 - Withdraw 
+                               4 - Change holders name 
+                               5 - Change PIN 
+                               6 - Exit""");
+            
+            System.out.print("Enter your option [1-6]: ");
+            opt = sc.nextInt();
+            switch(opt){
+                case 1:
+                    Utils.clearScreen();
+                    System.out.print("Balance: " + acc.getBalance() + "\u20ac");
+                    System.out.println();
+                    break;
+                case 2:
+                    Utils.clearScreen();
+                    acc.deposit();
+                    break;
+                case 3:
+                    Utils.clearScreen();
+                    acc.withdraw();
+                    break;
+                case 4:
+                    Utils.clearScreen();
+                    acc.changeHolderName();
+                    break;
+                case 5:
+                    Utils.clearScreen();
+                    acc.changePin();
+                    break;
+            }
+            
+            if(opt == 6){
+                System.out.println();
+                break;
+            }
+            System.out.print("Press any key to move forward: ");
+            sc.next();
+            Utils.clearScreen();
+        }
+        System.out.println("Thank you for using Maze Bank services!");
     }
 }
